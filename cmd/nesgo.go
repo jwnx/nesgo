c4353070686a630062794f7341f5061d66fb4977
Juliana Oliveira <juliana.orod@gmail.com>
2019-09-10 19:12:07 -0300

    nesgo: Allow printing supported instructions

--- cmd/nesgo.go
+++ cmd/nesgo.go
@@ -8,7 +8,13 @@ import (
 
 func main() {
 	romPath := flag.String("rom", "TOOLY/tooly.nes", "path to rom file")
+	printInstructions := flag.Bool("instructions", false, "displays all of the supported instructions")
 	flag.Parse()
+
+	if *printInstructions {
+		nesgo.PrintInstructions()
+		return
+	}
 
 	if _, _, err := nesgo.LoadiNESFile(romPath); err != nil {
 		log.Printf("Failed to load %s: %s", *romPath, err)
