f9c4cafbca9358243430c71d8cd59dea0f442713
Juliana Oliveira <juliana.orod@gmail.com>
2019-10-24 00:13:27 -0300

    nesgo/ppu: Add ppumstatus register emulation

--- nesgo/pkg/ppu.go
+++ nesgo/pkg/ppu.go
@@ -307,3 +307,45 @@ func (mask *ppumask) write(value byte) {
 	}
 }
 
+// 7  bit  0
+// ---- ----
+// VSO. ....
+// |||| ||||
+// |||+-++++- Least significant bits previously written into a PPU register
+// |||        (due to register not being updated for this address)
+// ||+------- Sprite overflow. The intent was for this flag to be set
+// ||         whenever more than eight sprites appear on a scanline, but a
+// ||         hardware bug causes the actual behavior to be more complicated
+// ||         and generate false positives as well as false negatives; see
+// ||         PPU sprite evaluation. This flag is set during sprite
+// ||         evaluation and cleared at dot 1 (the second dot) of the
+// ||         pre-render line.
+// |+-------- Sprite 0 Hit.  Set when a nonzero pixel of sprite 0 overlaps
+// |          a nonzero background pixel; cleared at dot 1 of the pre-render
+// |          line.  Used for raster timing.
+// +--------- Vertical blank has started (0: not in vblank; 1: in vblank).
+//            Set at dot 1 of line 241 (the line *after* the post-render
+//            line); cleared after reading $2002 and at dot 1 of the
+//            pre-render line.
+type ppustatus struct {
+	spriteOverflow bool
+	spiteZeroHit   bool
+	vblankStarted  bool
+}
+
+func (status *ppustatus) read(lastRegisterWrite byte, internal *internalRegisters) byte {
+	res := lastRegisterWrite & 0x1F
+	if status.spriteOverflow {
+		res |= 1 << 5
+	}
+	if status.spiteZeroHit {
+		res |= 1 << 6
+	}
+	if status.vblankStarted {
+		res |= 1 << 7
+		status.vblankStarted = false
+	}
+	internal.w = 0
+	return res
+}
+
