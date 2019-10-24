85cb1cb31e0096649e098712bea0171b40bd7114
Juliana Oliveira <juliana.orod@gmail.com>
2019-10-24 00:32:16 -0300

    nesgo: Add nes file to hold the console components

---
+++ nesgo/pkg/nes.go
@@ -0,0 +1,22 @@
+package nesgo
+
+// Cycles that a step function took
+type Cycles = uint
+
+// NES represents a virtual NES console, with mapper 0
+type NES struct {
+	CPU        *CPU
+}
+
+// NewNES returns a virtual NES console
+func NewNES(cartridge *Cartridge) *NES {
+	nes := NES{
+		CPU:        NewCPU(cartridge.PRG),
+	}
+	return &nes
+}
+
+// Reset triggers the CPU reset interrupt vector
+func (nes *NES) Reset() {
+	nes.CPU.Reset()
+}
